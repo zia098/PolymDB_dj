e24b52e383ab78c26a512204a61b0cb2fba4a794
zia098 <93577551+zia098@users.noreply.github.com>
2025-02-08 02:30:35 +0300

    Update settings.py

--- polymdb_p/settings.py
+++ polymdb_p/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,8 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-import os
-from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent # Base directory of the project
 
