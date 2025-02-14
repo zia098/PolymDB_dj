501d3bd2540c68fde066f8ffce0a306f635fd6a3
zia098 <93577551+zia098@users.noreply.github.com>
2025-02-14 21:54:36 +0300

    Update views.py

--- polymes/views.py
+++ polymes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import WildTypePolymerase, ModifiedPolymerase, FusionDomain
 from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 
 
@@ -14,3 +15,17 @@ def polymerase_list(request):
         'modifieds': modifieds,
         'fusions': fusions,
     })
+
+
+def wildtype_detail(request, pk):
+    polymerase = get_object_or_404(WildTypePolymerase, pk=pk)
+    return render(request, 'polymes/wildtype_detail.html', {'polymerase': polymerase})
+
+def modified_detail(request, pk):
+    polymerase = get_object_or_404(ModifiedPolymerase, pk=pk)
+    return render(request, 'polymes/modified_detail.html', {'polymerase': polymerase})
+
+def domain_detail(request, pk):
+    domain = get_object_or_404(FusionDomain, pk=pk)
+    return render(request, 'polymes/domain_detail.html', {'domain': domain})
+    
