65a862e1f4ec03125d19fc561bb2168aa559e59f
zia098 <93577551+zia098@users.noreply.github.com>
2025-02-14 21:51:56 +0300

    Update urls.py

--- polymes/urls.py
+++ polymes/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import polymerase_list
+from . import views
+from .views import polymerase_list, wildtype_detail, modified_detail, domain_detail
 
 
 
 urlpatterns = [
     path('', polymerase_list, name='polymerase_list'),
+
+    path('wildtype/<int:pk>/', wildtype_detail, name='wildtype_detail'),
+    path('modified/<int:pk>/', modified_detail, name='modified_detail'),
+    path('domain/<int:pk>/', domain_detail, name='domain_detail'),
 ]
