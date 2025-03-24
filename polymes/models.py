812e01dc2d7f744be4ecfe5083e52800d8d24b9a
zia098 <93577551+zia098@users.noreply.github.com>
2025-03-24 04:02:16 +0300

    Update models.py

--- polymes/models.py
+++ polymes/models.py
@@ -15,7 +15,7 @@ class WildTypePolymerase(models.Model):
 
     class Meta:
         db_table = "Wild_type_Polymerases"
-
+'''
 class ModifiedPolymerase(models.Model):
     id = models.AutoField(primary_key=True)  # Sync with MySQL
     name = models.TextField(db_column="Polymerase_Name", blank=True)
@@ -41,7 +41,40 @@ class ModifiedPolymerase(models.Model):
     doi = models.TextField(db_column="DOI", null=True, blank=True)
 
     class Meta:
-        db_table = "Modified_Polymerases"
+        db_table = "Modified_Polymerases"'
+'''
+
+class ModifiedPolymerase(models.Model):
+    id = models.AutoField(primary_key=True)  # Sync with MySQL
+    name = models.TextField(db_column="Polymerase_Name", blank=True)
+    base_polymerase = models.TextField(db_column="Base_Polymerase", null=True,blank=True)
+    modification_type = models.TextField(db_column="Modification_Type", null=True,blank=True)
+    modifications = models.TextField(db_column="Modifications", null=True,blank=True)
+    optimal_temp = models.TextField(db_column="Optimal_Temperature_c", null=True,blank=True)
+    melting_temp = models.TextField(db_column="Melting_Temperature_c", null=True,blank=True)
+    processivity = models.TextField(db_column="Processivity_bp", null=True,blank=True)
+    binding_affinity = models.TextField(db_column="Binding_Affinity_kd", null=True,blank=True)
+    ph_stability = models.TextField(db_column="pH_Stability_Range", null=True,blank=True)
+    optimal_ph = models.TextField(db_column="Optimal_pH", null=True, blank=True)
+    salt_tolerance_nacl = models.TextField(db_column="Salt_Tolerance_mM_NaCl", null=True, blank=True)
+    salt_tolerance_kcl = models.TextField(db_column="Salt_Tolerance_mM_KCI", null=True, blank=True)
+    activity = models.TextField(db_column="Activity", null=True, blank=True)
+    exo_activity_5_3 = models.TextField(db_column="exo_activity_5_3", null=True, blank=True)
+    exo_activity_3_5 = models.TextField(db_column="exo_activity_3_5", null=True, blank=True)
+    enhanced_properties = models.TextField(db_column="Enhanced_Properties", null=True, blank=True)
+    #general_error = models.TextField(db_column="General_Error", null=True, blank=True)
+    applications = models.TextField(db_column="Applications", null=True)
+    genbank = models.TextField(db_column="GenBank", null=True)
+    uniprot = models.TextField(db_column="UniProt", null=True)
+    PDB = models.TextField(db_column="PDB", null=True)
+    title = models.TextField(db_column="TITLE", null=True, blank=True)
+    doi = models.TextField(db_column="DOI", null=True, blank=True)
+
+    class Meta:
+        db_table = "Batch_1"
+
+
+
 
 class FusionDomain(models.Model):
     id = models.AutoField(primary_key=True)  # Sync with MySQL
@@ -56,5 +89,3 @@ class FusionDomain(models.Model):
 
     class Meta:
         db_table = "Fusion_Domains"
-
-
