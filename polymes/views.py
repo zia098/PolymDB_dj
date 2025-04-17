f041dde5e72237e28d77f25dbeba6bc12a3a31d5
zia098 <93577551+zia098@users.noreply.github.com>
2025-04-17 07:15:56 +0300

    Update views.py

--- polymes/views.py
+++ polymes/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
-from .models import WildTypePolymerase, ModifiedPolymerase, FusionDomain
-from django.http import JsonResponse
+# polymes/views.py
+
 from django.shortcuts import render, get_object_or_404
-
-
+from django.core.paginator import Paginator
+from .models import WildTypePolymerase, ModifiedPolymerase, FusionDomain
 
 def polymerase_list(request):
-    wild_types = WildTypePolymerase.objects.all()
-    modifieds = ModifiedPolymerase.objects.all()
-    fusions = FusionDomain.objects.all()
+    # fetch & order
+    wild_qs     = WildTypePolymerase.objects.all().order_by('name')
+    modified_qs = ModifiedPolymerase.objects.all().order_by('name')
+    fusion_qs   = FusionDomain.objects.all().order_by('name')
 
-    return render(request, 'polymes/list.html', {
-        'wild_types': wild_types,
-        'modifieds': modifieds,
-        'fusions': fusions,
-    })
+    # paginate (10 per page)
+    wild_paginator     = Paginator(wild_qs, 10)
+    modified_paginator = Paginator(modified_qs, 10)
+    fusion_paginator   = Paginator(fusion_qs, 10)
 
+    # pull page numbers from querystring
+    wild_page     = request.GET.get('wild_page')
+    modified_page = request.GET.get('mod_page')
+    fusion_page   = request.GET.get('fusion_page')
+
+    context = {
+        'wild_types': wild_paginator.get_page(wild_page),
+        'modifieds' : modified_paginator.get_page(modified_page),
+        'fusions'   : fusion_paginator.get_page(fusion_page),
+    }
+    return render(request, 'polymes/list.html', context)
 
 def wildtype_detail(request, pk):
     polymerase = get_object_or_404(WildTypePolymerase, pk=pk)
@@ -28,4 +38,4 @@ def modified_detail(request, pk):
 def domain_detail(request, pk):
     domain = get_object_or_404(FusionDomain, pk=pk)
     return render(request, 'polymes/domain_detail.html', {'domain': domain})
-    
+
