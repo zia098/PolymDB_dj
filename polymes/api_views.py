6896b8c9ab6fc712586a49d1f4f602a1cb313d8e
zia098 <93577551+zia098@users.noreply.github.com>
2025-06-23 02:48:49 +0300

    Update api_views.py

--- polymes/api_views.py
+++ polymes/api_views.py
@@ -2,13 +2,22 @@
 from rest_framework import generics
 from .models import ModifiedPolymerase
 from .serializers import ModifiedPolymeraseSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-class PolymeraseListCreateAPIView(generics.ListCreateAPIView):
 
-    queryset = ModifiedPolymerase.objects.all()
-    serializer_class = ModifiedPolymeraseSerializer
+
+
 
 class PolymeraseDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = ModifiedPolymerase.objects.all()
     serializer_class = ModifiedPolymeraseSerializer
+
+
+class PolymeraseListCreateAPIView(generics.ListCreateAPIView):
+    queryset = ModifiedPolymerase.objects.all()
+    serializer_class = ModifiedPolymeraseSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['base_polymerase', 'modification_type', 'optimal_temp']
+    search_fields    = ['name', 'modifications']
+    ordering_fields  = ['optimal_temp', 'activity']
