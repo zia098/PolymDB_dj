050bcac03fa75d447e4393687992c0cdd1fba3f6
zia098 <93577551+zia098@users.noreply.github.com>
2025-06-23 02:44:46 +0300

    Update urls.py

--- polymes/urls.py
+++ polymes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import polymerase_list
 from . import views
 from .views import polymerase_list, wildtype_detail, modified_detail, domain_detail
-
+from .api_views import PolymeraseListCreateAPIView, PolymeraseDetailAPIView
 
 
 urlpatterns = [
@@ -11,4 +11,6 @@ urlpatterns = [
     path('wildtype/<int:pk>/', wildtype_detail, name='wildtype_detail'),
     path('modified/<int:pk>/', modified_detail, name='modified_detail'),
     path('domain/<int:pk>/', domain_detail, name='domain_detail'),
+    path('api/polymerases/', PolymeraseListCreateAPIView.as_view(), name='api-polymerase-list-create'),
+    path('api/polymerases/<int:pk>/', PolymeraseDetailAPIView.as_view(), name='api-polymerase-detail'),
 ]
