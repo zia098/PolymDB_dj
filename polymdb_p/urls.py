8dcd045ddf7f4a4e5de0fd32e4d3830f19455b8e
zia098 <93577551+zia098@users.noreply.github.com>
2025-06-23 03:27:37 +0300

    Update urls.py

--- polymdb_p/urls.py
+++ polymdb_p/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('polymes/', include('polymes.urls')),
-    path('', include('polymdb_p.urls')),
+    path('polymes/', include('polymes.urls')),
+    #path('', include('polymdb_p.urls')),
 ]
